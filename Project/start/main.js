995982e03dfe2b60587262117d793a26452ecbcf
brucekumarsingh07 <rajputbatman07@gmail.com>
2025-06-07 16:06:04 +0530

    Add files via upload

---
+++ Project/start/main.js
@@ -0,0 +1,6 @@
+// This will hold JS for dynamic content in future
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("JS Loaded");
+
+  // You can later fetch data for news or user from API/backend here
+});
